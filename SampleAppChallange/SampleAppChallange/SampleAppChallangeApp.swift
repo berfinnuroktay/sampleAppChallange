1c7ac958faa89885d8d4ee7af3395e253353191b
Berfinnur Oktay <berfinnur.oktay@gmail.com>
2025-07-02 00:44:35 +0300

    feat: Add initial project

---
+++ SampleAppChallange/SampleAppChallange/SampleAppChallangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppChallangeApp.swift
+//  SampleAppChallange
+//
+//  Created by berf on 2.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppChallangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
